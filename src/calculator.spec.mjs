7a2b82bb9222789fb7722a5ebf5ebc2121b69ff8
leelanarasimha <leela.narsimha@gmail.com>
2022-01-16 19:46:27 +0530

    Add test

--- src/calculator.spec.mjs
+++ src/calculator.spec.mjs
@@ -4,4 +4,7 @@ describe('calculator.js', function () {
   it('should add two numbers', function () {
     expect(add(1, 2)).toBe(3);
   });
+  it('should add two numbers', function () {
+    expect(add(1, 2)).toBe(3);
+  });
 });
